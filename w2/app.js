c489ac26029b7b53c710918a6b2532d60ebdea83
Phatsakone Phanthoulay <142254871+bigtouonly@users.noreply.github.com>
2026-08-18 01:46:11 +0000

    declaring varibles

--- w2/app.js
+++ w2/app.js
@@ -1,1 +1,17 @@
 console.log ('Hello from app.js! Your JavaScript is connected and working!');
+// --- Part 1: Declaring Variables ---
+console.log('--- Part 1: Declaring Variables');
+
+// 'let' for values that can change
+// We can reassign 'let' variables
+let score = 50;
+console.log(`Initial score (let): ${score}`);
+score = 75;
+console.log(`Updated score: ${score}`);
+
+// 'const' for values that should NOT change
+const GAME_TITLE = "Super Mario Bros";
+console.log(`Game Title (const): ${GAME_TITLE}`);
+// GAME_TITLE = "Another Mario Game";
+// Comment out above line error
+
