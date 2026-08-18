bdd532ca364abc06b554e905be1f45369d1a0a13
Phatsakone Phanthoulay <142254871+bigtouonly@users.noreply.github.com>
2026-08-18 02:25:50 +0000

    Data Types

--- w2/app.js
+++ w2/app.js
@@ -15,3 +15,16 @@ console.log(`Game Title (const): ${GAME_TITLE}`);
 // GAME_TITLE = "Another Mario Game";
 // Comment out above line error
 
+// --- Part 2: Data Types ---
+console.log("--- Data Types (using typeof) ---");
+
+let playerName = "Hero";      // String (text)
+let playerHealth = 100;         // number (whole or decimal number)
+let isGameOver = false;         // boolean (true or false)
+
+console.log(`${playerName} is a typeof ${typeof playerName}`);
+console.log(`${playerHealth} is a typeof ${typeof playerHealth}`);
+console.log(`${isGameOver} is a typeof ${typeof isGameOver}`);
+
+// Phatsakone's Variable Rule
+// Use const unless you can't than use let
